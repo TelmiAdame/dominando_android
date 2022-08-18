a057917eb3a52cb732f1724a6615b707042b1790
Telmi Adame <telmiarquit@hotmail.com>
2022-08-18 19:57:16 +0100

    update

--- app/src/main/java/com/tadame/dominandoandroiid/MainActivity.kt
+++ app/src/main/java/com/tadame/dominandoandroiid/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tadame.dominandoandroiid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tadame.dominandoandroiid.databinding.ActivityMainBinding
+import android.content.Intent
 import android.widget.Toast
 
 
@@ -15,12 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)     //declara o layout
-        setContentView(binding.root)                            //inicializa
+        setContentView(binding.root)                            //inicia layout
         setContentView(R.layout.activity_main)
 
         binding.buttonToast.setOnClickListener {
-            val texto = binding.editTexto.text
+            val texto = binding.editTexto.text.toString()
             Toast.makeText(this, texto, Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonTela2.setOnClickListener {
+            val intent = Intent(this, Tela2Activity::class.java)
+            intent.putExtra("nome", "Glauber")
+            intent.putExtra("idade", 35)
+            startActivity(intent)
         }
 
     }
